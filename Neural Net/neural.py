d4374f836862579744937ffe713274c720fa312b
Parv <parvagrw02@gmail.com>
2021-01-23 17:10:19 +0530

    Nice its making a little sense

--- Neural Net/neural.py
+++ Neural Net/neural.py
@@ -26,24 +26,48 @@ class Layer:
     
     nodes = []
 
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, number):
         for i in range(input_size):
-            nodes.append(Node())
-
-            #Initialize weights
-            nodes[i].set_weight(random.randrange(0,1))
+            nodes.append(Node(input_size, []))
 
 
 class Node:
-    #Each node has a weight and an array of node connections and the layer number
-    weight = 0.0
-    connection = []
 
-    def __init__(self):
-        weight = 0
+    #Each node has an array of weights and an array of inputs
+    weights = []
+    inputs = []
+
+    #Make sure that inputs is an array of Nodes, would have been cooler in java
+    def __init__(self, n_weights, inputs, first):
+        
+        self.inputs = inputs
+
+        #initial state is off
+        self.state = 0
+        self.first = first
+
+        #Randomly initializing the weights
+        for i in range(n_weights):
+            weights.append(random.randint(0,10000)/10000)
+
+    def next_state(activation):
+        
+        z = 0
+
+        for i in len(weights){
+            
+            if(self.first):
+                z += inputs[i] * weights[i]
+            else:
+                z += inputs[i].state * weights[i]
+        }
+
+        #Update state based on inputs, weights and the activation function
+        self.state = activation(z)
+
+        return self.state
+
+    #Create a list of random nodes
+    def list_nodes():
+
     
-    def set_weight(self, val):
-        self.weight = val
-    
-    def weight(self):
-        return weight 
