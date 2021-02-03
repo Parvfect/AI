42a26e754cd226014ea6e4fd7c9ff4dba729f37c
Parv <parvagrw02@gmail.com>
2021-02-03 17:12:07 +0530

    Forward pass done?

--- Neural Net/scratch.py
+++ Neural Net/scratch.py
@@ -1,11 +1,41 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
+import numpy as np 
+import nnfs
+from nnfs.datasets import spiral_data
 
-inputs = np.array(np.random.rand(10))
-weights = np.array(np.random.rand(10))
-bias = 3
 
-output = np.dot(inputs, weights) + bias
+nnfs.init()
 
-print(output)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
