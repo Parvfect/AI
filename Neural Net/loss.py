e0b62a3a219c4279a6ac7a2df1a0cd1bba59c5e4
Parv <parvagrw02@gmail.com>
2021-02-04 17:09:19 +0530

    Actually finished a forward pass

--- Neural Net/loss.py
+++ Neural Net/loss.py
@@ -1,6 +1,21 @@
 
 import math
 
+
+
+# calculate mean squared error
+def mean_squared_error(actual, predicted):
+    sum_square_error = 0.0
+    for i in range(len(actual)):
+        sum_square_error += (actual[i] - predicted[i])**2.0
+    mean_square_error = 1.0 / len(actual) * sum_square_error
+    return mean_square_error
+
+
+    """
+def logloss(self,output, target):
+    return np.log(self.)
+
 softmax_output = [0.7, 0.1, 0.2]
 target_output = [1, 0, 0]
 
@@ -12,3 +27,4 @@ print(loss)
 
 print(-math.log(0.7))
 print(-math.log(0.5))
+"""
